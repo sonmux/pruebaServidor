15b3050d042bdb23767d35fbd54aabcdda58ec78
sonmux <son.anggelo@gmail.com>
2023-09-15 21:44:59 -0600

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 const db = new Sequelize('recepcion', 'root', '1234',{
-    host:'localhost',
-    dialect:'mysql'
+    host:'127.0.0.1', //localhost
+    dialect:'mysql',
+    port: 3306
 });
 
 export default db
